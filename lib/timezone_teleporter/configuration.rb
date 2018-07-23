8ac9ee6241cc669bf63450c1dde29dcc4c84c853
Ole Richter <ole@blinkist.com>
2018-07-23 14:51:20 +0200

    Revert "feature/introduce logger in configuration (#5)"
    This reverts commit cf6d35e7f0de22c51e2b68e6ea4fd9f609f018cb.

--- lib/timezone_teleporter/configuration.rb
+++ lib/timezone_teleporter/configuration.rb
@@ -2,16 +2,14 @@
 
 module TimezoneTeleporter
   class Configuration
+    attr_accessor :silent_mode
     attr_accessor :use_proximity_algorithm
     attr_accessor :delta_degree
 
-    attr_accessor :logger
-
     def initialize
+      @silent_mode = true
       @use_proximity_algorithm = true
       @delta_degree = 1
-
-      @logger = ::Logger.new(nil)
     end
   end
 end
