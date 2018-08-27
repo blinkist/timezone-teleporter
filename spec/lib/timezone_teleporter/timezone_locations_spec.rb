015dc0c1fe4ec18ae31319d167b07477bd438348
Ole Richter <ole@blinkist.com>
2018-08-27 12:00:31 +0200

    Thanks, rubocop

--- spec/lib/timezone_teleporter/timezone_locations_spec.rb
+++ spec/lib/timezone_teleporter/timezone_locations_spec.rb
@@ -5,11 +5,11 @@ require "spec_helper"
 RSpec.describe TimezoneTeleporter do
   context "::TIMEZONE_LOCATIONS" do
     let(:excluded_locations) { ["Asia/Yangon", "Asia/Famagusta"] }
-    
+
     let(:locations) {
-      TimezoneTeleporter::TIMEZONE_LOCATIONS.select { |t, c| !excluded_locations.include? t }
+      TimezoneTeleporter::TIMEZONE_LOCATIONS.reject { |t, _c| excluded_locations.include? t }
     }
-    
+
     it "locations matches timezones" do
       locations.each do |timezone, coordinates|
         expect(TimezoneTeleporter.timezone_at(*coordinates)).to eq(timezone)
